39e134c0901df3c1473172722ef8148e9bc43ccd
Randy DuCharme <radio.ad5gb@gmail.com>
2016-07-06 20:47:21 -0500

    Revert "Compatibility with GCC5.x"
    This reverts commit cdf063c1104c4fafabb4cc003a8555b262c24d23.

--- drivers/gpu/msm/kgsl_iommu.c
+++ drivers/gpu/msm/kgsl_iommu.c
@@ -1010,8 +1010,7 @@ static int kgsl_iommu_init_sync_lock(struct kgsl_mmu *mmu)
  *
  * Return - int - number of commands.
  */
-/* inline unsigned int kgsl_iommu_sync_lock(struct kgsl_mmu *mmu, */
-unsigned int kgsl_iommu_sync_lock(struct kgsl_mmu *mmu,
+inline unsigned int kgsl_iommu_sync_lock(struct kgsl_mmu *mmu,
 						unsigned int *cmds)
 {
 	struct kgsl_device *device = mmu->device;
@@ -1081,8 +1080,7 @@ unsigned int kgsl_iommu_sync_lock(struct kgsl_mmu *mmu,
  *
  * Return - int - number of commands.
  */
-/* inline unsigned int kgsl_iommu_sync_unlock(struct kgsl_mmu *mmu, */
-unsigned int kgsl_iommu_sync_unlock(struct kgsl_mmu *mmu,
+inline unsigned int kgsl_iommu_sync_unlock(struct kgsl_mmu *mmu,
 					unsigned int *cmds)
 {
 	struct kgsl_device *device = mmu->device;
